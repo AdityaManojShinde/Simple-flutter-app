337cb4f117ec2b563674343d26779765eaef37d9
AdityaManojShinde <aditya.manoj.shinde@gmail.com>
2023-07-27 07:51:14 +0530

    final commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,27 +1,37 @@
 import 'package:dino_tech/widgets/drawer.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool modeChange = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Row(
-          children: [Text('Dino Tech')],
+          children: [
+            Text('Dino Tech'), 
+          ],
         ),
       ),
       drawer: const AppDrawer(),
-      body: Container(
-        color: Colors.white,
-        child: Column(
-          children: [
-            Image.asset(
-              'assets/images/home2.jpg',
-              fit: BoxFit.cover,
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Container(
+          color: Colors.white,
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/images/home2.jpg',
+                fit: BoxFit.cover,
+              ),
+            ],
+          ),
         ),
       ),
     );
